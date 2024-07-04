eb2266aff71eb7fabfbcdf8c6e2b921fe8a041e9
INviZEo <lev19825@mail.ru>
2024-07-04 17:21:23 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getState());
+        first.start();
+        System.out.println(first.getName());
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(second.getName());
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+        }
+        try {
+            first.join();
+            second.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(first.getState());
+        System.out.println("Process is finish");
+    }
+}
