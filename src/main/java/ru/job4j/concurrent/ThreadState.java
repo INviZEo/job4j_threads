31d02dca9032adfa8eb4dbeaa1e25d58f9b2f600
INviZEo <lev19825@mail.ru>
2024-07-06 14:42:39 +0300

    3. Состояние нити. [#229175 #503757]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,25 +3,19 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {
-                }
+                () -> System.out.println(Thread.currentThread().getName())
         );
         System.out.println(first.getState());
         first.start();
-        System.out.println(first.getName());
         Thread second = new Thread(
-                () -> {
-                }
+                () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(second.getName());
+        second.start();
         while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
+            System.out.println("first = " + first.getState());
         }
-        try {
-            first.join();
-            second.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println("second = " + second.getState());
         }
         System.out.println(first.getState());
         System.out.println("Process is finish");
