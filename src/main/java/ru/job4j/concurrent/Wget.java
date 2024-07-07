dd30f0c897393557032a97cfb1ba328969b89717
INviZEo <lev19825@mail.ru>
2024-07-07 12:17:50 +0300

    4. Режим ожидания. [#231217 #503777]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,22 +1,18 @@
 package ru.job4j.concurrent;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Wget {
     public static void main(String[] args) {
-        final int[] i = {0};
         Thread first = new Thread(
                 () -> {
                     try {
-                        while (i[0] <= 100) {
-                            System.out.println("Loading: " + i[0] + "%");
-                            i[0] = Math.incrementExact(i[0]);
-                            Thread.sleep(2000);
+                        for (int i = 0; i <= 100; i++) {
+                            System.out.println("Loading: " + i + "%");
+                            Thread.sleep(1000);
                         }
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    System.out.println("File is download");
+                    System.out.println("File is downloaded");
                 }
         );
         first.start();
