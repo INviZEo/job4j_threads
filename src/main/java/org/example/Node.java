ec789d59463748f7cac8088d33d1c0382547b305
INviZEo <lev19825@mail.ru>
2024-07-19 18:22:16 +0300

    3. Immutable объекты [#267918]

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Node {
+//    private Node left;
+//    private Node right;
+//    private String value;
+//
+//    public Node getLeft() {
+//        return left;
+//    }
+//
+//    public void setLeft(Node left) {
+//        this.left = left;
+//    }
+//
+//    public Node getRight() {
+//        return right;
+//    }
+//
+//    public void setRight(Node right) {
+//        this.right = right;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
+//
+//    public boolean isEqual(Node treeOne, Node treeTwo) {
+//        boolean rsl = false;
+//
+//    }
+}
