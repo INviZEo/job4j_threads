54aa679d7695cc90a3f2692aa16435e5abd746ee
INviZEo <lev19825@mail.ru>
2025-02-25 22:03:16 +0300

    0. CAS - операции [#6859 #531006]

--- src/main/java/ru/job4j/concurrent/CASCount.java
+++ src/main/java/ru/job4j/concurrent/CASCount.java
@@ -3,19 +3,21 @@ package ru.job4j.concurrent;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
     private final AtomicInteger count = new AtomicInteger();
 
     public void increment() {
-        count.incrementAndGet();
-        throw new UnsupportedOperationException("Count is not impl.");
+        int currentValue;
+        int newValue;
+        do {
+            currentValue = count.get();
+            newValue = currentValue + 1;
+        } while (!count.compareAndSet(currentValue, newValue));
     }
 
     public int get() {
-        count.get();
-        throw new UnsupportedOperationException("Count is not impl.");
+       return count.get();
     }
 }
